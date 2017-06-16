247c69abe839ef27fb5d10eb295ecf4ee3037ebf
Rene Jean Corneille <corneille.r.j@gmail.com>
2017-06-16 09:50:43 +0800

    Update __init__.py

--- trendpy/__init__.py
+++ trendpy/__init__.py
@@ -21,3 +21,10 @@
 # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
+
+from __future__ import absolute_import
+
+from trendpy.globals import *
+
+from trendpy.version import version
+from trendpy.factory import StrategyFactory
