f32b54915e385d9aa36915b849ecf42198001583
Rene Jean Corneille <corneille.r.j@gmail.com>
2017-06-16 17:27:13 +0800

    Update version.py

--- trendpy/version.py
+++ trendpy/version.py
@@ -1,11 +1,11 @@
 
-    # FILE CONTENT GENERATED FROM SETUP.PY
+# FILE CONTENT GENERATED FROM SETUP.PY
 
-    short_version = '0.2.1'
-    version = '0.2.1'
-    full_version = '0.2.1'
-    release = False
+short_version = '0.3.1'
+version = '0.3.1'
+full_version = '0.3.1'
+release = False
 
-    if not release:
-        version = full_version
+if not release:
+	version = full_version
     
