ab7baaeee06f163f8ff16a1e4b89932ac16c4a01
Rene Jean Corneille <corneille.r.j@gmail.com>
2017-06-16 15:48:25 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -15,6 +15,17 @@ Links
 import os
 import sys
 from setuptools import setup, find_packages
+from distutils.sysconfig import get_python_lib
+
+if "install" in sys.argv:
+    lib_paths = [get_python_lib()]
+    if lib_paths[0].startswith("/usr/lib/"):
+        lib_paths.append(get_python_lib(prefix="/usr/local"))
+    for lib_path in lib_paths:
+        existing_path = os.path.abspath(os.path.join(lib_path, "trendpy"))
+        if os.path.exists(existing_path):
+            overlay_warning = True
+            break
 
 def get_version_info():
     """
@@ -24,8 +35,8 @@ def get_version_info():
     """
 
     MAJOR = 0
-    MINOR = 3
-    MICRO = 0
+    MINOR = 2
+    MICRO = 6
 
     ISRELEASED = False
 
@@ -95,7 +106,7 @@ def setup_package():
           zip_safe = False,
           include_package_data = True,
           platforms = 'any',
-		  package_dir={'trendpy': './trendpy'},
+		  #package_dir={'': ''},
 		  packages=['trendpy'],
           install_requires = ['numpy',
                               'scipy',
