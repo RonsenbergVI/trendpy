b0b4ce35e08131a2fbc87d412c7767c92db7e82e
Rene Jean Corneille <corneille.r.j@gmail.com>
2017-06-19 15:59:19 +0800

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -111,10 +111,21 @@ htmlhelp_basename = 'trendpydoc'
 # -- Options for LaTeX output ---------------------------------------------
 
 latex_elements = {
-    'fontpkg': r'\usepackage{mathpazo}',
-    'papersize': 'a4paper',
-    'pointsize': '12pt',
-    'preamble': r'\usepackage{flaskstyle}'
+    # The paper size ('letterpaper' or 'a4paper').
+    #
+    # 'papersize': 'letterpaper',
+
+    # The font size ('10pt', '11pt' or '12pt').
+    #
+    # 'pointsize': '10pt',
+
+    # Additional stuff for the LaTeX preamble.
+    #
+    # 'preamble': '',
+
+    # Latex figure (float) alignment
+    #
+    # 'figure_align': 'htbp',
 }
 
 # Grouping the document tree into LaTeX files. List of tuples
@@ -151,4 +162,4 @@ texinfo_documents = [
 
 
 # Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {'python': ('https://docs.python.org/3/', None)}
+intersphinx_mapping = {'https://docs.python.org/': None}
