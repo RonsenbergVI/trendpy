dab36263c17cbf12703355d1247fa980ea853d4f
RonsenbergVI <corneille.r.j@gmail.com>
2017-06-19 21:29:59 +0800

    added test files

--- setup.py
+++ setup.py
@@ -73,21 +73,6 @@ if not release:
     finally:
         _file.close()
 
-#def configuration(parent_package='',top_path=None):
-    #from numpy.distutils.misc_util import Configuration
-
-    #config = Configuration(None, parent_package, top_path)
-    #config.set_options(ignore_setup_xxx_py=True,
-    #                   assume_default_configuration=True,
-    #                   delegate_options_to_subpackages=True,
-    #                   quiet=True)
-
-    #config.add_subpackage('numpy')
-
-    #config.get_version('numpy/version.py') # sets config.version
-
-    #return config
-
 def setup_package():
 
     _version, _isReleased = get_version_info()
