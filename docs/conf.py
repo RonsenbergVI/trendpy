7c026be04e2b1351889ac2b2fa9532c695e5c7c1
Rene Jean Corneille <corneille.r.j@gmail.com>
2017-06-20 11:50:21 +0800

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -163,3 +163,5 @@ texinfo_documents = [
 
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {'https://docs.python.org/': None}
+
+auto_content = 'both'
