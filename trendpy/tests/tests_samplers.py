15d7a8565ac77848d595db6a9dfd0076c8bd9342
Rene Jean Corneille <corneille.r.j@gmail.com>
2017-06-30 17:22:33 +0800

    Update tests_samplers.py

--- trendpy/tests/tests_samplers.py
+++ trendpy/tests/tests_samplers.py
@@ -33,12 +33,53 @@ currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentfram
 parentdir = os.path.dirname(os.path.dirname(currentdir))
 sys.path.insert(0,parentdir)
 
-from trendpy.mcmc import *
+from trendpy.samplers import *
+
+from scipy.stats import rv_continuous, gamma
+
+from pandas import DataFrame, date_range
+
 
 class TestSamplers(unittest.TestCase):
 
-    def setUp(self):
-        pass
+	def setUp(self):
+		self.p = Parameter('test',rv_continuous,(10,10))
+		self.q = Parameter('test2',rv_continuous,(1,1))
+		self.P = Parameters()
+		dates = date_range(start='2014-01-01', end='2015-04-03',freq='D')
+		d = gamma.rvs(10,size=dates.size)
+		data = DataFrame(data=d, index=dates)
+		self.S = L1Filter(data)
 
-    def tearDown(self):
-        pass
+	def tearDown(self):
+		self.P.removeAll()
+
+	def test_empty_parameters(self):
+		print('empty')
+		self.assertEqual(self.P.list,{})
+		self.assertEqual(self.P.hierarchy,[])
+
+	def test_parameter_is_multivariate(self):
+		self.assertTrue(self.p.is_multivariate() and not self.q.is_multivariate())
+
+	def test_initial_current_value(self):
+		self.assertIsNone(self.p.current_value)
+
+	def test_add_parameter(self):
+		self.P.append(self.p)
+		self.assertTrue(self.p.name in self.P.list and self.p.name in self.P.hierarchy)
+		self.P.removeAll()
+		self.assertTrue(not self.p.name in self.P.list and not self.p.name in self.P.hierarchy)
+
+	def test_hierarchy(self):
+		self.P.append(self.p)
+		self.P.append(self.q)
+		self.assertTrue(self.P.hierarchy.index(self.p.name) < self.P.hierarchy.index(self.q.name))
+
+	def test_parameters_distributions(self):
+		for name in self.S.parameters.hierarchy:
+			self.assertTrue(super(self.S.parameters.list[name].distribution)==rv_continuous or )
+		
+if __name__ == "__main__":
+	unittest.main()
+	
