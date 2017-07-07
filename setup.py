5fb99f7d9693d7548e6c59e4d4ced64266a9f592
Rene Jean Corneille <corneille.r.j@gmail.com>
2017-07-07 17:55:57 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ trendpy
 -----
 
 trendpy is a trend filtering microframework
-trendpy implements L1, L2 and Lp filtering solved by MCMC methods
+trendpy implements L1 filtering solved by MCMC methods
 
 -----
 Links
@@ -18,6 +18,9 @@ import os
 import sys
 from setuptools import setup, find_packages
 from distutils.sysconfig import get_python_lib
+from configparser import ConfigParser
+
+config_file = os.path.join(os.path.dirname(__file__), 'setup.cfg')
 
 if "install" in sys.argv:
     lib_paths = [get_python_lib()]
@@ -27,51 +30,51 @@ if "install" in sys.argv:
         existing_path = os.path.abspath(os.path.join(lib_path, "trendpy"))
 
 def get_version_info():
-    """
-    get_version_info
-    ------------
-    gets software version infos from
-    """
+	"""
+	get_version_info
+	------------
+	gets software version infos from
+	"""
+	config=ConfigParser()
+	config.read(config_file)
+	MAJOR=config['version']['major']
+	MINOR=config['version']['minor']
+	MICRO=config['version']['micro']
 
-    MAJOR = 0
-    MINOR = 1
-    MICRO = 1
+	ISRELEASED = True
+	
+	VERSION = '%s.%s.%s' % (MAJOR, MINOR, MICRO)
 
-    ISRELEASED = False
+	return VERSION, ISRELEASED
 
-    VERSION = '%d.%d.%d' % (MAJOR, MINOR, MICRO)
+def write_version_py(filename="version.py"):
+	"""
+	write_version_py
+	------------
+	write software version infos in version.py file
+	"""
+	file_content = """ 
+	 # FILE CONTENT GENERATED FROM SETUP.PY
+	short_version = '%(version)s'
+	version = '%(version)s'
+	full_version = '%(full_version)s'
+	release = %(isrelease)s
 
-    return VERSION, ISRELEASED
+	if not release:
+		version = full_version
+	"""
+	filename = os.path.join(os.path.dirname(__file__), 'trendpy/%s' % filename)
+	
+	VERSION, ISRELEASED = get_version_info()
 
-def write_version_py(filename="trendpy/version.py"):
-    """
-    write_version_py
-    ------------
-    write software version infos in version.py file
-    """
+	_file = open(filename,'w+')
 
-    file_content = """
-# FILE CONTENT GENERATED FROM SETUP.PY
-
-short_version = '%(version)s'
-version = '%(version)s'
-full_version = '%(full_version)s'
-release = %(isrelease)s
-
-if not release:
-	version = full_version
-    """
-
-    VERSION, ISRELEASED = get_version_info()
-
-    _file = open(filename,"w")
-
-    try:
-        _file.write(file_content % {'version': VERSION,
+	try:
+		_file.write(file_content % {'version': VERSION,
                        'full_version' : VERSION,
                        'isrelease': str(ISRELEASED)})
-    finally:
-        _file.close()
+	finally:
+		_file.close()
 
 def setup_package():
 
@@ -90,7 +93,6 @@ def setup_package():
           zip_safe = False,
           include_package_data = True,
           platforms = 'any',
-		  #package_dir={'': ''},
 		  packages=['trendpy'],
           install_requires = ['numpy',
                               'scipy',
